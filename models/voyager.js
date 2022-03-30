ad783bb617c9fd9b85fd7314198e0641a1b84029
kathleendiep <kathleendiep@gmail.com>
2022-03-29 17:23:49 -0700

    added heroku

--- models/voyager.js
+++ models/voyager.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const voyagerSchema = new Schema({
+    name: {type: String, required: true},
     location: {type: String, required: true},
     address: {type: String, required: false},
     category: {type: String, required: true},
